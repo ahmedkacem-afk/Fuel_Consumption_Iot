06d5eda918011ca8959c65d74cdb1c07fbf1bf6e
Hamdi Ben Mansour <hamdibm331@gmail.com>
2024-11-17 00:50:08 +0100

    feat: adding the fuel consumption widget

--- frontend/src/components/dashboardLayout.js
+++ frontend/src/components/dashboardLayout.js
@@ -1,17 +1,16 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
+import * as React from "react";
+import Box from "@mui/material/Box";
 
-import { createTheme } from '@mui/material/styles';
-import { AppProvider } from '@toolpad/core/AppProvider';
-import { DashboardLayout } from '@toolpad/core/DashboardLayout';
-import { useDemoRouter } from '@toolpad/core/internal';
-import Bermil from './fuelLevel';
+import { createTheme } from "@mui/material/styles";
+import { AppProvider } from "@toolpad/core/AppProvider";
+import { DashboardLayout } from "@toolpad/core/DashboardLayout";
+import { useDemoRouter } from "@toolpad/core/internal";
+import Bermil from "./fuelLevel";
 import MaintenanceReminder from "./maintenanceReminder";
-
+import { LineChart } from "@mui/x-charts";
 const demoTheme = createTheme({
- 
   cssVariables: {
-    colorSchemeSelector: 'data-toolpad-color-scheme',
+    colorSchemeSelector: "data-toolpad-color-scheme",
   },
   colorSchemes: { light: true, dark: true },
   breakpoints: {
@@ -21,7 +20,6 @@ const demoTheme = createTheme({
       md: 600,
       lg: 1200,
       xl: 1536,
-      
     },
   },
 });
@@ -30,42 +28,54 @@ function DemoPageContent() {
 return (
     <Box
         sx={(theme) => ({
-            minHeight: '54rem',
-            padding: theme.spacing(3,0),
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'flex-start',
-            backgroundColor: theme.palette.mode === 'light' ? '#EEEEEE' : theme.palette.background.default,
+            padding: theme.spacing(3, 0, -4, 0),
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "flex-start",
+            backgroundColor:
+                theme.palette.mode === "light"
+                    ? "#EEEEEE"
+                    : theme.palette.background.default,
+            height: "100vh",
+            overflow: "hidden",
         })}
     >
-        <div className='flex flex-col ml-8'>
-            <Bermil/>
-            <MaintenanceReminder reminderDate="20-12-2024"/>
-        </div>
+        
+            <div className="grid grid-col-1 grid-flow-rows gap-4 lg:grid-rows-2 lg:grid-flow-col ml-8 h-full overflow-auto">
+                <Bermil />
+                <MaintenanceReminder reminderDate="20-12-2024" />
+            <div className="bg-slate-200 rounded-xl shadow-xl lg:ml-3">
+                <h2 className="text-center font-sans font-bold mt-8 text-2xl">Fuel Consumption</h2>
+                <LineChart
+                    xAxis={[{ data: [1, 2, 3,4, 5, 8, 10,11,12,13,14,15,16,17,18,19,20,21,22,23,24] ,label: "hours"}]}
+                    series={[
+                        {
+                            data: [30, 8.8, 8.5, 8.2, 8.1],label: "Fuel Consumption (litres)"
+                        },
+                    ]}
+                    width={500}
+                    height={300}
+                />
+            </div>
+            </div>
+            
         
     </Box>
 );
 }
 
-DemoPageContent.propTypes = {
-
-};
+DemoPageContent.propTypes = {};
 
 function DashboardLayoutSidebarHidden() {
-  
-
-  const router = useDemoRouter('/fuelLevel');
-
-  
+  const router = useDemoRouter("/fuelLevel");
 
   return (
     <AppProvider router={router} theme={demoTheme}>
       <DashboardLayout hideNavigation>
-        <DemoPageContent/>
+        <DemoPageContent />
       </DashboardLayout>
     </AppProvider>
   );
 }
-
 
 export default DashboardLayoutSidebarHidden;
--- frontend/src/components/maintenanceReminder.js
+++ frontend/src/components/maintenanceReminder.js
@@ -3,8 +3,8 @@ import Lottie from 'react-lottie';
 import animationData from '../ressources/calender.json';
 const MaintenanceReminder = ({ reminderDate }) => {
     return (
-        <div className="items-center bg-slate-200 rounded-xl shadow-xl p-8 mt-8 ">
-            <Lottie
+        <div className="items-center bg-slate-200 rounded-xl shadow-xl  mt-8 h-fit">
+            <Lottie 
                 options={{
                     loop: false,
                     autoplay: true,
@@ -14,7 +14,7 @@ const MaintenanceReminder = ({ reminderDate }) => {
                 width={150}
             />
             <h2 className='font-bold text-center text-black'>Maintenance Reminder</h2>
-            <p className="text-center text-black">{reminderDate}</p>
+            <p className="text-center text-black pb-4">{reminderDate}</p>
         </div>
     );
 };
